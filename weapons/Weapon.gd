f71ca805e708c85ce303e490b529b4d4a3975663
Guilherme Henrique Teixeira de Oliveira <htoguilherme@gmail.com>
2018-12-08 16:18:20 -0200

    Weapon selection

--- weapons/Weapon.gd
+++ weapons/Weapon.gd
@@ -3,6 +3,8 @@ class_name Weapon
 
 export(int, 1, 10) var fire_rate : int = 1
 export var ammo : int = 15
+export var damage : float = 5.0
+export(int, 0, 3) var selection_index : = 0
 
 onready var timer : = $Timer as Timer
 onready var animation : = $AnimationPlayer as AnimationPlayer
@@ -14,7 +16,17 @@ func initialize(raycast : RayCast) -> void:
 
 func _ready() -> void:
 	timer.wait_time = 1.0 / fire_rate
+	connect('tree_entered', self, '_on_tree_entered')
+	connect('tree_exiting', self, '_on_tree_exiting')
+	animation.play('pickup')
 
+func _on_tree_entered() -> void:
+	animation.play('pickup')
+
+func drop() -> void:
+	animation.play('drop')
+	yield(animation, 'animation_finished')
+	
 func _process(delta : float) -> void:
 	if Input.is_action_pressed('shoot') and timer.is_stopped() and ammo > 0:
 		timer.start()
--- weapons/WeaponsManager.gd
+++ weapons/WeaponsManager.gd
@@ -4,5 +4,46 @@ class_name WeaponsManager
 onready var aim : = $Aim as RayCast
 onready var hand : = $Hand
 
+var selected_index : int = 0
+var weapons = [
+	null,
+	null,
+	null,
+	null
+]
+
 func _ready() -> void:
-	hand.get_child(0).initialize(aim)
+	var initial_weapon = hand.get_child(0) as Weapon
+	initial_weapon.initialize(aim)
+	selected_index = initial_weapon.selection_index
+	weapons[selected_index] = initial_weapon
+
+func _process(delta : float) -> void:
+	if Input.is_action_just_pressed('ui_accept'):
+		add_weapon(load("res://weapons/Shotgun.tscn"))
+	
+	if Input.is_action_pressed('select_pistol'):
+		switch_weapon(0)
+	elif Input.is_action_pressed('select_shotgun'):
+		switch_weapon(1)
+	elif Input.is_action_pressed('select_machine_gun'):
+		switch_weapon(2)
+	elif Input.is_action_pressed('select_grenade_launcher'):
+		switch_weapon(3)
+
+func add_weapon(weapon) -> void:
+	var new_weapon = weapon.instance()
+	new_weapon.initialize(aim)
+	weapons[new_weapon.selection_index] = new_weapon
+	yield(hand.get_child(0).drop(), 'completed')
+	hand.remove_child(hand.get_child(0))
+	hand.add_child(new_weapon)
+
+func switch_weapon(index : int) -> void:
+	if weapons[index] == null or index == selected_index:
+		return
+	selected_index = index
+	yield(hand.get_child(0).drop(), 'completed')
+	hand.remove_child(hand.get_child(0))
+	hand.add_child(weapons[index])
+	
