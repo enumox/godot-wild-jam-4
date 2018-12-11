dac71d890d1b67b5aaaf8e39061a5a7fab9fd340
Guilherme Henrique Teixeira de Oliveira <htoguilherme@gmail.com>
2018-12-11 16:53:08 -0200

    Weapons meshes and behaviors added

---
+++ weapons/machine-gun/MachineGun.gd
@@ -0,0 +1,10 @@
+extends "res://weapons/Weapon.gd"
+
+func shoot() -> void:
+	if not animation.is_playing():
+		animation.play('rotate')
+	ammo -= 1
+	if raycast.is_colliding():
+		var enemy = raycast.get_collider() as Enemy
+		if enemy != null:
+			enemy.take_damage(damage)
---
+++ weapons/shotgun/Shotgun.gd
@@ -0,0 +1,14 @@
+extends "res://weapons/Weapon.gd"
+
+export var max_hit_distance : float
+export(float, 0.1, 0.75) var max_distance_modifier : float = 0.25
+
+func shoot() -> void:
+	animation.play('shoot')
+	ammo -= 1
+	if raycast.is_colliding():
+		var enemy = raycast.get_collider() as Enemy
+		if enemy != null:
+			var distance = enemy.global_transform.origin.distance_to(global_transform.origin)
+			var modifier = 1.25 - distance / max_hit_distance if distance < max_hit_distance else max_distance_modifier
+			enemy.take_damage(damage * modifier)
